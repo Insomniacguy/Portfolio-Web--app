5decb61234b74e8ad9c9dc18775ddb8199535c50
InsomniacGuy <rahulchandan1991@gmail.com>
2024-04-13 09:56:58 +0530

    Add new content below 2 cols

--- main.py
+++ main.py
@@ -4,12 +4,22 @@ st.set_page_config(layout="wide")
 
 col1, col2 = st.columns(2)
 
+
 with col1:
     st.image('Images/me with glasses.png')
 
 with col2:
     st.title("Rahul Chandan")
-    content = """I'm Rahul, a freelance Python developer. My background spans online marketing, cybersecurity, 
-    and healthcare. With expertise across various business domains, I excel in understanding your specific use cases."""
+    content = """I'm Rahul, a freelance Python developer. I completed my Bachelors of Science in Information 
+    Technology from Mumbai University in 2017. Over the years, I've gained experience in diverse fields such as 
+    online marketing, cybersecurity, and healthcare. My broad understanding of different business areas allows me to 
+    grasp your unique requirements effectively. I specialize in crafting tailored software solutions that prioritize 
+    security, scalability, and relevance to your business needs."""
     # st.write(content)
     st.info(content)
+
+content2 = """
+Below, you can find some of the apps that I have built. Feel free to contact me
+"""
+st.info(content2)
+
