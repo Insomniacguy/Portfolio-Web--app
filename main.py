85f5e04f4646e6f388ae6d703ac0c99bdae76b4e
InsomniacGuy <rahulchandan1991@gmail.com>
2024-04-13 12:47:33 +0530

    Render the titles

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import streamlit as st
+import pandas
 
+df = pandas.read_csv('data.csv', sep=';')
 st.set_page_config(layout="wide")
 
 col1, col2 = st.columns(2)
-
 
 with col1:
     st.image('Images/me with glasses.png')
@@ -23,3 +24,17 @@ Below, you can find some of the apps that I have built. Feel free to contact me
 """
 st.info(content2)
 
+st.write(df)
+
+col3, col4 = st.columns(2)
+
+# slicing used for diving the content in 2 columns
+with col3:
+    # for title in df["title"]:
+    #     st.header(title)
+    for index, row in df[:10].iterrows():  # slicing from 0 till 10. first 10 items
+        st.header(row["title"])
+
+with col4:
+    for index, row in df[10:].iterrows():  #starting from index 10. So last 10 items
+        st.header(row["title"])
