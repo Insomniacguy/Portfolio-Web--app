7b92b9da000e57e2c5194ed7cf37f41ec9fae9f8
InsomniacGuy <rahulchandan1991@gmail.com>
2024-04-15 14:42:22 +0530

    Send Emails through web form

--- pages/Contact_us.py
+++ pages/Contact_us.py
@@ -1,4 +1,22 @@
 import streamlit as st
-
+from send_email import send_email
 
 st.header("Contact Me")
+
+with st.form(key='form'):
+    user_email = st.text_input("Enter your email address", key='email')
+    raw_message = st.text_area("Enter your message")
+
+    message = f"""\
+Subject: New email from {user_email}
+
+From: {user_email}
+{raw_message}
+"""
+    button = st.form_submit_button("submit")
+    print(button)
+    if button:
+        # print("I was pressed")
+        send_email(message)
+        st.info("The message was sent")
+
