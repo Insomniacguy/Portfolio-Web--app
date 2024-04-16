904cc391131e08bc5bb7067b2336f802c5056ce1
InsomniacGuy <rahulchandan1991@gmail.com>
2024-04-16 07:25:25 +0530

    Added User Environment variables to make the code-base secure.

--- send_email.py
+++ send_email.py
@@ -1,5 +1,6 @@
 import smtplib
 import ssl
+import os
 
 
 def send_email(message):
@@ -7,22 +8,21 @@ def send_email(message):
     port = 465
     # unsecure way to store passwords. The more secure way is to store it in env variables
     username = "r00t7h3ll@gmail.com"
-    password = "trgixhavcuiffxdh"
+    # password = "trgixhavcuiffxdh"
+    password = os.getenv("PASSWORD")
     receiver = "rahulchandan1991@gmail.com"
     # subject = "Python rocks"
     # host, port, context
     # backslash indicates there is no new line. For subject
-#     message = """\
-# Subject: Python indeed rocks
-# Does this work??
-# Testing
-# Bye!
-# """
+    #     message = """\
+    # Subject: Python indeed rocks
+    # Does this work??
+    # Testing
+    # Bye!
+    # """
     context = ssl.create_default_context()
     with smtplib.SMTP_SSL(host, port, context=context) as server:
         server.login(username, password)
         server.sendmail(username, receiver, message)
 
-
 # send_email()
-
